a5410d473a954bcbce38379237273fa4141edf56
Phil Haack <haacked@gmail.com>
2023-08-28 09:59:48 -0700

    Implement basic Q&A

--- src/AiDemoWeb/Pages/Index.cshtml.cs
+++ src/AiDemoWeb/Pages/Index.cshtml.cs
@@ -1,37 +1,66 @@
-﻿using Haack.AIDemoWeb.Startup.Config;
+﻿using System.ComponentModel.DataAnnotations;
+using Haack.AIDemoWeb.Startup.Config;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Options;
 using OpenAI_API;
+using OpenAI_API.Chat;
 
 namespace AIDemoWeb.Pages;
 
 public class IndexModel : PageModel
 {
-    readonly ILogger<IndexModel> _logger;
     readonly OpenAIOptions _options;
 
-    public IndexModel(IOptions<OpenAIOptions> options, ILogger<IndexModel> logger)
+    public IndexModel(IOptions<OpenAIOptions> options)
     {
         _options = options.Value;
-        _logger = logger;
     }
 
     [BindProperty]
+    [Required]
     public string? Question { get; init; }
 
+    public string Answer { get; private set; } = string.Empty;
+
     [BindProperty]
-    public string? Model { get; init; }
+    [Required]
+    public string? OpenAIModel { get; init; }
 
     public IReadOnlyList<SelectListItem> Models { get; private set; } = Array.Empty<SelectListItem>();
 
     public async Task OnGetAsync()
+    {
+        await InitializePageAsync();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var client = await InitializePageAsync();
+
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        var chat = client.Chat.CreateConversation(new ChatRequest
+        {
+            Model = OpenAIModel
+        });
+        chat.AppendSystemMessage("Hello, you are a friendly chat bot who is part of a demo I'm giving and wants to represent me and Chat GPT well.");
+        chat.AppendUserInput(Question);
+        Answer = await chat.GetResponseFromChatbotAsync();
+
+        return Page();
+    }
+
+    async Task<OpenAIAPI> InitializePageAsync()
     {
         var client = new OpenAIAPI(new APIAuthentication(_options.ApiKey, _options.OrganizationId));
         var models = await client.Models.GetModelsAsync();
         Models = models
             .Select(m => new SelectListItem(m.ModelID, m.ModelID))
             .ToList();
+        return client;
     }
 }
