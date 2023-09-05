f9ea9463ab1cb0122dc9d5de334fa43ad06c0432
Phil Haack <haacked@gmail.com>
2023-09-05 14:32:06 -0700

    Add migration for user facts

--- src/AIDemoWeb/Migrations/20230905213146_AddUserFacts.Designer.cs
+++ src/AIDemoWeb/Migrations/20230905213146_AddUserFacts.Designer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Haack.AIDemoWeb.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Haack.AIDemoWeb.Migrations
 {
     [DbContext(typeof(AIDemoContext))]
-    partial class AIDemoContextModelSnapshot : ModelSnapshot
+    [Migration("20230905213146_AddUserFacts")]
+    partial class AddUserFacts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AIDemoWeb/Migrations/20230905213146_AddUserFacts.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Haack.AIDemoWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFacts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Embeddings = table.Column<List<float>>(type: "real[]", nullable: false),
+                    Justification = table.Column<string>(type: "text", nullable: false),
+                    Source = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserFacts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFacts_UserId_Content",
+                table: "UserFacts",
+                columns: new[] { "UserId", "Content" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFacts");
+        }
+    }
+}
