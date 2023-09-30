0c73a1ae62011cc059a01520be2c834cdb0cfd12
Phil Haack <haacked@gmail.com>
2023-09-30 14:31:19 -0700

    Display the currently used models
    And switch to gpt-4.

--- src/AIDemoWeb/Pages/Demos/Models.cshtml.cs
+++ src/AIDemoWeb/Pages/Demos/Models.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Azure.AI.OpenAI;
 using Haack.AIDemoWeb.Library.Clients;
+using Haack.AIDemoWeb.Startup.Config;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using Serious;
 
 namespace AIDemoWeb.Demos.Pages;
@@ -11,10 +13,16 @@ public class ModelsPageModel : PageModel
 {
     readonly OpenAIClientAccessor _client;
 
-    public ModelsPageModel(OpenAIClientAccessor clientAccessor)
+    public ModelsPageModel(OpenAIClientAccessor clientAccessor, IOptions<OpenAIOptions> options)
     {
         _client = clientAccessor;
+        CompletionsModel = options.Value.Require().Model;
+        EmbeddingsModel = options.Value.Require().EmbeddingModel;
     }
+
+    public string CompletionsModel { get; }
+
+    public string EmbeddingsModel { get; }
 
     public IReadOnlyList<OpenAIEntity> Models { get; private set; } = null!;
 
