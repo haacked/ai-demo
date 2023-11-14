0fe64b4dfaf7d9313665d9abd3db6b5bfb9965cc
Phil Haack <haacked@gmail.com>
2023-11-13 20:24:34 -0800

    Use the pgvector docker image

--- docker/postgres/1-init-user-and-db.sh
+++ docker/postgres/1-init-user-and-db.sh
@@ -2,7 +2,5 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER aidemouser WITH PASSWORD 'mtUvGABu';
-    CREATE DATABASE aidemo;
-    GRANT ALL PRIVILEGES ON DATABASE aidemo TO aidemo;
+    CREATE DATABASE aidemodb;
 EOSQL
