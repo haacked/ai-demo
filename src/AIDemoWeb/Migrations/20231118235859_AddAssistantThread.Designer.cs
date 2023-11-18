9483afbfd36ed5d62c3d0dea02bdb3a87b339ef5
Phil Haack <haacked@gmail.com>
2023-11-18 15:59:20 -0800

    Add Migration for an Assistant thread

--- src/AIDemoWeb/Migrations/20231118235859_AddAssistantThread.Designer.cs
+++ src/AIDemoWeb/Migrations/20231118235859_AddAssistantThread.Designer.cs
@@ -2,6 +2,7 @@
 using Haack.AIDemoWeb.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pgvector;
@@ -11,9 +12,11 @@ using Pgvector;
 namespace Haack.AIDemoWeb.Migrations
 {
     [DbContext(typeof(AIDemoContext))]
-    partial class AIDemoContextModelSnapshot : ModelSnapshot
+    [Migration("20231118235859_AddAssistantThread")]
+    partial class AddAssistantThread
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AIDemoWeb/Migrations/20231118235859_AddAssistantThread.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Haack.AIDemoWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssistantThread : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Threads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ThreadId = table.Column<string>(type: "text", nullable: false),
+                    CreatorId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Threads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Threads_Users_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Threads_CreatorId",
+                table: "Threads",
+                column: "CreatorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Threads");
+        }
+    }
+}
