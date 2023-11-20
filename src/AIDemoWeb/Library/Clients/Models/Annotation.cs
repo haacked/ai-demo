d36ce881a64733e70e15caaa843a0a6c2f6a00f9
Phil Haack <haacked@gmail.com>
2023-11-19 20:20:38 -0800

    Implement citation within document

--- src/AIDemoWeb/Library/Clients/Models/Annotation.cs
+++ src/AIDemoWeb/Library/Clients/Models/Annotation.cs
@@ -1,10 +1,27 @@
 namespace Haack.AIDemoWeb.Library.Clients;
 
+/// <summary>
+/// An annotation associated with message content.
+/// </summary>
+/// <param name="Type">Either <c>file_citation</c> or <c>file_path</c>.</param>
+/// <param name="Text">The text in the message content that needs to be replaced.</param>
+/// <param name="StartIndex">Index of the start of the citation.</param>
+/// <param name="EndIndex">Index of the end of the citation.</param>
+/// <param name="FileCitation">
+/// A citation within the message that points to a specific quote from a specific File associated with the assistant
+/// or the message. Generated when the assistant uses the "retrieval" tool to search files. This is only present if
+/// <see cref="Type"/> is <c>file_citation</c>.
+/// </param>
+/// <param name="FilePath">
+/// A URL for the file that's generated when the assistant used the code_interpreter tool to generate a file. Only
+/// present when <see cref="Type"/> is <c>file_path</c>.
+/// </param>
 public record Annotation(
     string Type,
     string Text,
     int StartIndex,
     int EndIndex,
-    FileCitation FileCitation);
+    FileCitation? FileCitation,
+    FileReference? FilePath);
 
-public record FileCitation(string FileId, string Quote);
+public record FileCitation(string FileId, string Quote) : FileReference(FileId);
