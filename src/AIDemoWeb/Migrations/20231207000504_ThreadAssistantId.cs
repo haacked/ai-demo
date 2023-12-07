8ebb96b1037633b0950ac1ed0607d4a6bd55a208
Phil Haack <haacked@gmail.com>
2023-12-06 16:05:31 -0800

    Add Thread AssistantId Migration

---
+++ src/AIDemoWeb/Migrations/20231207000504_ThreadAssistantId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Haack.AIDemoWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThreadAssistantId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssistantId",
+                table: "Threads",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssistantId",
+                table: "Threads");
+        }
+    }
+}
