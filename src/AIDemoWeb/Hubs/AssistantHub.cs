e7f3db864ff68074523cd3e731019227611a5e42
Phil Haack <haacked@gmail.com>
2023-12-21 11:10:41 -0800

    Set up logging for AssistantHub.

--- src/AIDemoWeb/Hubs/AssistantHub.cs
+++ src/AIDemoWeb/Hubs/AssistantHub.cs
@@ -8,10 +8,12 @@ namespace OpenAIDemo.Hubs;
 public class AssistantHub : Hub
 {
     readonly IPublishEndpoint _publishEndpoint;
+    readonly ILogger<AssistantHub> _logger;
 
-    public AssistantHub(IPublishEndpoint publishEndpoint)
+    public AssistantHub(IPublishEndpoint publishEndpoint, ILogger<AssistantHub> logger)
     {
         _publishEndpoint = publishEndpoint;
+        _logger = logger;
     }
 
     public async Task Broadcast(string message, bool isUser, string assistantName, string assistantId, string threadId)
@@ -22,13 +24,28 @@ public class AssistantHub : Hub
 
     public override Task OnConnectedAsync()
     {
-        Console.WriteLine($"{Context.ConnectionId} connected");
+        _logger.Connected(Context.ConnectionId);
         return base.OnConnectedAsync();
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        Console.WriteLine($"Disconnected {exception?.Message} {Context.ConnectionId}");
+        _logger.Disconnected(exception, Context.ConnectionId);
         await base.OnDisconnectedAsync(exception);
     }
+}
+
+public static partial class AssistantHubLoggingExtensions
+{
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Information,
+        Message = "{ConnectionId} connected")]
+    public static partial void Connected(this ILogger<AssistantHub> logger, string connectionId);
+
+    [LoggerMessage(
+        EventId = 2,
+        Level = LogLevel.Information,
+        Message = "{ConnectionId} disconnected")]
+    public static partial void Disconnected(this ILogger<AssistantHub> logger, Exception? exception, string connectionId);
 }
