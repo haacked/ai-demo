5c3a10c014596e0a17cb47fe52f7e35bedea990b
Phil Haack <haacked@gmail.com>
2024-03-25 13:39:42 -0700

    Add migration of User location info

---
+++ src/AiDemoWeb/Migrations/20240325203927_AddLocationToUser.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace Haack.AIDemoWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:citext", ",,")
+                .Annotation("Npgsql:PostgresExtension:postgis", ",,")
+                .Annotation("Npgsql:PostgresExtension:vector", ",,")
+                .OldAnnotation("Npgsql:PostgresExtension:citext", ",,")
+                .OldAnnotation("Npgsql:PostgresExtension:vector", ",,");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FormattedAddress",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Point>(
+                name: "Location",
+                table: "Users",
+                type: "geometry (point)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZoneId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FormattedAddress",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TimeZoneId",
+                table: "Users");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:citext", ",,")
+                .Annotation("Npgsql:PostgresExtension:vector", ",,")
+                .OldAnnotation("Npgsql:PostgresExtension:citext", ",,")
+                .OldAnnotation("Npgsql:PostgresExtension:postgis", ",,")
+                .OldAnnotation("Npgsql:PostgresExtension:vector", ",,");
+        }
+    }
+}
