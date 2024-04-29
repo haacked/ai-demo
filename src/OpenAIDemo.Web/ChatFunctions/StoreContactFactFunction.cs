87f762ee7d93de01ae8d262bf856b048de184c30
Phil Haack <haacked@gmail.com>
2024-04-29 12:35:34 -0700

    Reimplement functions to store facts
    These functions now store and retrieve facts for contacts, not users.

--- src/OpenAIDemo.Web/ChatFunctions/StoreContactFactFunction.cs
+++ src/OpenAIDemo.Web/ChatFunctions/StoreContactFactFunction.cs
@@ -3,29 +3,30 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Azure.AI.OpenAI;
 using Haack.AIDemoWeb.Entities;
-using Haack.AIDemoWeb.Library;
 using Microsoft.EntityFrameworkCore;
-using NetTopologySuite.Geometries;
 using Pgvector;
+using Serious;
+using Serious.ChatFunctions;
+// ReSharper disable All
 
-namespace Serious.ChatFunctions;
+namespace Haack.AIDemoWeb.ChatFunctions;
 
 /// <summary>
-/// Extends Chat GPT with a function that stores facts about a user.
+/// Extends Chat GPT with a function that stores facts about a contact.
 /// </summary>
-public class StoreUserFactFunction(AIDemoContext db, OpenAIClientAccessor client, GeocodeClient geocodeClient)
-    : ChatFunction<UserFactArguments, object>
+public class StoreContactFactFunction(AIDemoContext db, OpenAIClientAccessor client)
+    : ChatFunction<ContactFactArguments, object>
 {
-    protected override string Name => "store_user_fact";
+    protected override string Name => "store_contact_fact";
 
     protected override string Description =>
         """
-        Stores information about a person when a user makes a declarative statement or statements about another person or themself. 
+        Stores information about a contact when a user makes a declarative statement or statements about another person. 
         This only responds to declarative statements.
 
-        For example, the statement \"@haacked's favorite color is blue\"
+        For example, the statement \"David Fowler's favorite color is blue\"
 
-        results in the fact \"favorite color is blue\" stored for the username \"haacked\".
+        results in the fact \"favorite color is blue\" stored for the name \"David Fowler\".
 
         If no username is specified, use the `retrieve_user_fact` function to retrieve the username.
 
@@ -35,57 +36,31 @@ public class StoreUserFactFunction(AIDemoContext db, OpenAIClientAccessor client
     public int Order => 2; // Comes after the StoreUserRelationshipFunction
 
     protected override async Task<object?> InvokeAsync(
-        UserFactArguments arguments,
+        ContactFactArguments arguments,
         string source,
         CancellationToken cancellationToken)
     {
-        var username = arguments.Username.TrimLeadingCharacter('@');
-
-        var user = await db.Users
-            .Include(u => u.Facts)
-            .FirstOrDefaultAsync(u => u.NameIdentifier == username, cancellationToken);
-
-        if (user is null)
+        var contact = await GetContactByNameAsync(arguments.ContactName, cancellationToken);
+        if (contact is null)
         {
-            user = new User
-            {
-                NameIdentifier = username,
-            };
-            await db.Users.AddAsync(user, cancellationToken);
+            return new { Message = "I don't know that contact" };
         }
 
         foreach (var fact in arguments.Facts)
         {
             if (
                 // Make sure it's not already stored verbatim.
-                !user.Facts.Any(f => f.Content.Equals(fact, StringComparison.OrdinalIgnoreCase)))
+                !contact.Facts.Any(f => f.Content.Equals(fact, StringComparison.OrdinalIgnoreCase)))
             {
                 // Generate chat embedding for fact.
                 var embeddings = await GetEmbeddingsAsync(fact, cancellationToken);
 
-                if (arguments.Location is { Coordinate: { } coordinate } location)
-                {
-                    user.Location = new Point(coordinate.Latitude, coordinate.Longitude)
-                    {
-                        // 4326 is the SRID for the WGS84 spatial reference system, which is commonly used for latitude
-                        // and longitude coordinates.
-                        SRID = 4326
-                    };
-                    user.FormattedAddress = location.FormattedAddress;
-
-                    if (await geocodeClient.GetTimeZoneAsync(coordinate.Latitude,
-                            coordinate.Longitude)
-                        is { } timeZoneId)
-                    {
-                        user.TimeZoneId = timeZoneId;
-                    }
-                }
 
                 // TODO: Let's ask GPT if this fact is already known or changes an existing fact.
-                user.Facts.Add(new UserFact
+                contact.Facts.Add(new ContactFact
                 {
-                    User = user,
-                    UserId = user.Id,
+                    Contact = contact,
+                    ContactId = contact.Id,
                     Content = fact,
                     Embeddings = new Vector(embeddings),
                     Justification = arguments.Justification,
@@ -96,6 +71,13 @@ public class StoreUserFactFunction(AIDemoContext db, OpenAIClientAccessor client
         }
 
         return null; // No need to respond.
+    }
+
+    Task<Contact?> GetContactByNameAsync(string contactName, CancellationToken cancellationToken)
+    {
+        return db.Contacts
+            .Include(c => c.Facts)
+            .FirstOrDefaultAsync(c => c.Names.Any(n => EF.Functions.ILike(n.UnstructuredName, contactName)), cancellationToken);
     }
 
     async Task<float[]> GetEmbeddingsAsync(string fact, CancellationToken cancellationToken)
@@ -126,21 +108,16 @@ public class StoreUserFactFunction(AIDemoContext db, OpenAIClientAccessor client
 /// <summary>
 /// The arguments to the store user fact function.
 /// </summary>
-public record UserFactArguments(
+public record ContactFactArguments(
     [property: Required]
-    [property: JsonPropertyName("username")]
-    [property: Description("The username used to uniquely identify a user.")]
-    string Username,
+    [property: JsonPropertyName("contact_name")]
+    [property: Description("The name of the contact.")]
+    string ContactName,
 
     [property: Required]
     [property: JsonPropertyName("facts")]
-    [property: Description("The facts to store about the user.")]
+    [property: Description("The facts to store about the contact.")]
     IReadOnlyList<string> Facts,
-
-    [property: Required]
-    [property: JsonPropertyName("location")]
-    [property: Description("The location where the user lives if the fact is about the user's location. The `location_info` can be used to get this.")]
-    UserLocation? Location,
 
     [property: Required]
     [property: JsonPropertyName("justification")]
