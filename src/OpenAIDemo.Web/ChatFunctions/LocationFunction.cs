4d907f8ea487862ac38a53d4b4a0ad5766e036da
Phil Haack <haacked@gmail.com>
2024-04-30 10:57:24 -0700

    Remove outdated code

--- src/OpenAIDemo.Web/ChatFunctions/LocationFunction.cs
+++ src/OpenAIDemo.Web/ChatFunctions/LocationFunction.cs
@@ -1,11 +1,9 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using Haack.AIDemoWeb.Entities;
 using Haack.AIDemoWeb.Library;
 using Haack.AIDemoWeb.Library.Clients;
 using Haack.AIDemoWeb.Startup.Config;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Serious;
 using Serious.ChatFunctions;
@@ -17,13 +15,12 @@ namespace Haack.AIDemoWeb.ChatFunctions;
 /// </summary>
 public class LocationFunction(
     IGoogleGeocodeClient geocodeClient,
-    AIDemoContext db,
     IOptions<GoogleOptions> geocodeOptions)
     : ChatFunction<ContactLocationArguments, ContactLocation>
 {
     protected override string Name => "location_info";
 
-    protected override string Description => "Retrieves location info any time a user mentions a location or address. For example, the statement \"I live at 123 Main St\" results in the location info for 123 Main St being retrieved. When asking about my own location, look it up based on my username.";
+    protected override string Description => "Retrieves location info any time a user mentions a location or address. For example, the statement \"I live at 123 Main St\" results in the location info for 123 Main St being retrieved.";
 
     public int Order => 1;
 
@@ -36,15 +33,9 @@ public class LocationFunction(
         var response = await geocodeClient.GeoCodeAsync(apiKey, arguments.Address);
         if (response.Results is { Count: 0 })
         {
-            var username = arguments.Address.StartsWith('@') ? arguments.Address.TrimStart('@') : arguments.Address;
-
-            // Maybe the address was a username. Look up the user's location.
-            var contacts = await db.Users
-                .FirstOrDefaultAsync(u => u.NameIdentifier == username, cancellationToken);
-            return contacts is { Location: { } location, FormattedAddress: { } formattedAddress }
-                ? new ContactLocation(new Coordinate(location.Coordinate.Y, location.Coordinate.X), formattedAddress)
-                : new ContactLocation(new Coordinate(0, 0), "Unknown");
+            return new ContactLocation(new Coordinate(0, 0), "Unknown");
         }
+
         var result = response.Results[0];
 
         return new ContactLocation(
