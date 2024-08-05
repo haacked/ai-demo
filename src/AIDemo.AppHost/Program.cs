f59097ccd95cf85f523e0954b5e8a5c6c55bf1a3
Phil Haack <haacked@gmail.com>
2024-08-05 09:56:21 -0700

    Got ngrok running as a resource

--- src/AIDemo.AppHost/Program.cs
+++ src/AIDemo.AppHost/Program.cs
@@ -14,4 +14,10 @@ builder.AddProject<AIDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(postgresdb);
 
+builder.AddExecutable(
+        name: "ngrok",
+        command: "ngrok",
+        workingDirectory: ".",
+        "tunnel", "--label", "edge=edghts_2UcgMuXxfrdN8OEN1oPK8W4kAB7", "https://localhost:7047");
+
 builder.Build().Run();
