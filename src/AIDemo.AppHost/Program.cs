97751cb55ff3e66b5c0b618d65629ec9d64efed0
Phil Haack <haacked@gmail.com>
2024-08-06 15:36:58 -0700

    Only run ngrok in development

--- src/AIDemo.AppHost/Program.cs
+++ src/AIDemo.AppHost/Program.cs
@@ -16,10 +16,14 @@ builder.AddProject<AIDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(postgresdb);
 
-builder.AddExecutable(
+if (!builder.ExecutionContext.IsPublishMode)
+{
+    // This is only for development.
+    builder.AddExecutable(
         name: "ngrok",
         command: "ngrok",
         workingDirectory: ".",
         "tunnel", "--label", "edge=edghts_2UcgMuXxfrdN8OEN1oPK8W4kAB7", "https://localhost:7047");
+}
 
 builder.Build().Run();
