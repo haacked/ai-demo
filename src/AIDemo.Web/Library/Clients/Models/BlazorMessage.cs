afd35c5c16f154163a5957da1cdbe4181a0d36fa
Phil Haack <haacked@gmail.com>
2024-09-16 13:43:19 -0700

    Remove rendering of text annotations

--- src/AIDemo.Web/Library/Clients/Models/BlazorMessage.cs
+++ src/AIDemo.Web/Library/Clients/Models/BlazorMessage.cs
@@ -1,9 +1,6 @@
-using OpenAI.Assistants;
-
 namespace AIDemo.Library.Clients;
 
 public record BlazorMessage(
     string Text,
     bool IsUser,
-    DateTime Created,
-    IReadOnlyList<TextAnnotation>? Annotations = null);
+    DateTime Created);
