22b78aa3a0d701cd6ca2cb803d4e02e23a5ab1b5
Phil Haack <haacked@gmail.com>
2025-04-19 12:58:34 -0700

    Update scripts
    Add outdated script

--- script/helpers/_utils.sh
+++ script/helpers/_utils.sh
@@ -6,3 +6,10 @@ fatal() {
     error "$@"
     exit 1
 }
+
+set_source_and_root_dir() {
+    { set +x; } 2>/dev/null
+    source_dir="$( cd -P "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+    root_dir=$(cd "$source_dir" && cd ../ && pwd)
+    cd "$root_dir"
+}
