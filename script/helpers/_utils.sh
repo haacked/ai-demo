a7fe02f37ce20e5bc69887b68d9e7accd8ad4ca8
Phil Haack <haacked@gmail.com>
2025-04-20 14:48:00 -0700

    Streamline installing dependencies

--- script/helpers/_utils.sh
+++ script/helpers/_utils.sh
@@ -13,3 +13,17 @@ set_source_and_root_dir() {
     root_dir=$(cd "$source_dir" && cd ../ && pwd)
     cd "$root_dir"
 }
+
+install_if_missing() {
+    if ! type "$1" >/dev/null 2>&1; then
+        echo "Installing $1"
+        if [ -n "$2" ]; then
+            echo "Reason: $2"
+        else
+            echo "Reason: $1 is required to run the project."
+        fi
+        brew install "$1"
+    else
+        echo "✅$1 installed."
+    fi
+}
